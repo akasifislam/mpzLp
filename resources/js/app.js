c36ab78b7bd7d04333c4e80a1cb5c1a473ceeb02
akasifislam <aseasifislam@gmail.com>
2021-03-25 21:40:20 +0600

    class practic two

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
 require('./bootstrap');
+
+window.Vue = require('vue');
+// import Vue from 'vue'
+// import Header from './components/Header.vue';
+import Myheader from './components/Myheader.vue';
+
+const app = new Vue({
+    el: '#app',
+    components:{
+        Myheader
+    }
+});
---
+++ resources/js/components/Myheader.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid facilis error necessitatibus impedit aut atque iure dolorum laboriosam corrupti, dolor vitae quibusdam quas consectetur nulla, velit nesciunt expedita minima itaque!</div>
+</template>
